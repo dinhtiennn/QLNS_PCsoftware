84059f18c115871f1dcfb4ac0df70ea6691ef581
Nessit2610 <letien26102003@gmail.com>
2024-04-20 09:42:03 +0700

    TienLe

--- src/main/java/bo/NhanVienBo.java
+++ src/main/java/bo/NhanVienBo.java
@@ -1,5 +1,22 @@
 package bo;
 
-public class NhanVienBo {
+import java.util.ArrayList;
 
+import bean.NhanVienBean;
+import dao.NhanVienDao;
+
+
+public class NhanVienBo {
+	ArrayList<NhanVienBean> ds;
+	NhanVienDao nvd = new NhanVienDao();
+	public ArrayList<NhanVienBean> getnhanvien() throws Exception{
+		ds = nvd.getnhanvien();
+		return ds;
+	}
+	public NhanVienBean getnhanvientheoma(String manv) throws Exception{
+		return nvd.getnhanvientheoma(manv);
+	}
+	public NhanVienBean getNhanVienTheoTKvaMK( String tendangnhap,String matkhau) throws Exception{
+		return nvd.getNhanVienTheoTKvaMK(tendangnhap,matkhau);
+	}
 }
---
+++ src/main/java/controller/index.java
@@ -0,0 +1,65 @@
+package controller;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.NhanVienBean;
+
+import bo.NhanVienBo;
+
+
+/**
+ * Servlet implementation class index
+ */
+@WebServlet("/index")
+public class index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public index() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String select_Nam = request.getParameter("select-nam");
+		String select_Thang = request.getParameter("selectthang");
+		NhanVienBo nvbo = new NhanVienBo();
+	
+		NhanVienBean nv = (NhanVienBean)session.getAttribute("nhanvien");
+		String url = "";
+		if(nv != null) {
+			url = "ADMINview.jsp";
+			
+		}else {
+			url = "login.jsp";
+		}
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
