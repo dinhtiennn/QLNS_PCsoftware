d5a9d491e838a7087266f20644904a369695182e
NgoTan2003 <ngotan2k3@gmail.com>
2024-05-03 18:30:11 +0700

    tanmoipush3-3

--- src/main/java/admindao/ketnoiDB.java
+++ src/main/java/admindao/ketnoiDB.java
@@ -7,15 +7,7 @@ public class ketnoiDB {
 	public static Connection cn;
 	public void ketnoi() throws Exception {
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-<<<<<<< HEAD
-<<<<<<< HEAD
 		String url ="jdbc:sqlserver://DESKTOP-VSQGE62\\TANNGO:1433;databaseName=QLNS; user=sa; password=123";
-=======
-		String url ="jdbc:sqlserver://RIN\\SQLEXPRESS:1433;databaseName=QLNS; user=sa; password=123";
->>>>>>> 097148eef5fefc12e245ad24425f83197efdf3c1
-=======
-		String url ="jdbc:sqlserver://RIN\\SQLEXPRESS:1433;databaseName=QLNS; user=sa; password=123";
->>>>>>> 097148eef5fefc12e245ad24425f83197efdf3c1
 		cn = DriverManager.getConnection(url);
 		System.out.println("Da Ket Noi");
 	}
